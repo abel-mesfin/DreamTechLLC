28753c903fbba134805319cebcec1056dace7d65
abel-mesfin <73157783+abel-mesfin@users.noreply.github.com>
2022-02-10 17:48:59 -0700

    Update myfuncs.php

--- myfuncs.php
+++ myfuncs.php
@@ -10,9 +10,9 @@ The modules purpose is to create a html form that shows the user the required fi
 
 function dbConnect() {
 //Mysql connection info
-$servername = "localhost";
-$username = "root";
-$password = "root";
+$servername = "127.0.0.1:54939";
+$username = "azure";
+$password = "6#vWHD_$";
 $dbname = "activity1";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
